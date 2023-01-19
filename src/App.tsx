5d0e1034f29ff0ea341d58889cae89aae2ab6cc9
kajolroy <shrikajol@gmail.com>
2023-01-19 21:51:52 +0600

    jest to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
-import React, {useEffect} from 'react';
-type Props ={
-    children:React.ReactNode;
-    callback:()=>void;
-    bottomToActionPosition?:number;
-    getDataLength:number
+import React, { useEffect } from 'react'
+interface Props {
+  children: React.ReactNode
+  callback: () => void
+  bottomToActionPosition?: number
+  getDataLength?: number
 }
-const InfiniteScrollAction = (props:Props) => {
+const InfiniteScrollAction = (props: Props) => {
+  const { callback, getDataLength, bottomToActionPosition } = props
+  const minus = bottomToActionPosition || 5
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      const { scrollTop, clientHeight, scrollHeight } = document.documentElement
+      if (scrollTop + clientHeight > scrollHeight - minus) {
+        callback()
+      }
+    })
+  }, [getDataLength])
 
-    const {callback,getDataLength,bottomToActionPosition}=props
-    const minus = bottomToActionPosition||5
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            const { scrollTop, clientHeight, scrollHeight } =
-                document.documentElement;
-            if (scrollTop + clientHeight >scrollHeight -minus) {
-                callback()
-            }
-        });
-    }, [getDataLength]);
+  return <React.Fragment>{props.children}</React.Fragment>
+}
 
-    return (
-        <React.Fragment>
-            {props.children}
-        </React.Fragment>
-    );
-};
-
-export default InfiniteScrollAction;
+export default InfiniteScrollAction
